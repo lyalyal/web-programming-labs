b90772d025de85463126ae636992809226401f5c
Alina <alina.khizhnyak.2000@gmail.com>
2026-03-23 12:47:28 +0200

    Lab 5: Add todo update and delete

--- lab5/src/App.tsx
+++ lab5/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { todosApi } from "./api/todos";
 
 export default function App() {
+  //Стан зберігання
   const [title, setTitle] = useState("");
+  //Доступ для керування кешом
   const queryClient = useQueryClient();
-
   //Отримання даних
   const {
     data: todos,
@@ -15,27 +16,44 @@ export default function App() {
     queryKey: ["todos"],
     queryFn: todosApi.getAll,
   });
+  //Створення запитів
   const createTodo = useMutation({
-    mutationFn: () => todosApi.create({ title, completed: false }),
+    mutationFn: () =>
+      todosApi.create({
+        title,
+        completed: false,
+      }),
+    //Оновлення спискі після успішного виконання
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      });
+
       setTitle("");
     },
   });
-
-  //Оноволення завдання
+  //Оновлення запиту
   const updateTodo = useMutation({
     mutationFn: ({ id, completed }: { id: number; completed: boolean }) =>
       todosApi.update(id, { completed }),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["todos"] }),
+    //Оновлення даних щоб побачити зміни
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      }),
   });
-
+  //Видалення завдання
   const deleteTodo = useMutation({
     mutationFn: (id: number) => todosApi.remove(id),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["todos"] }),
+    //Повністю видалення задачі із кешу
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ["todos"],
+      }),
   });
 
   if (isLoading) return <p>Завантаження</p>;
+
   if (isError) return <p>Помилка</p>;
 
   return (
@@ -43,6 +61,7 @@ export default function App() {
       <h2>Список задач</h2>
 
       <input value={title} onChange={(e) => setTitle(e.target.value)} />
+
       <button
         onClick={() => createTodo.mutate()}
         disabled={createTodo.isPending}
@@ -55,13 +74,18 @@ export default function App() {
           <input
             type="checkbox"
             checked={todo.completed}
-            onChange={() =>
-              updateTodo.mutate({ id: todo.id, completed: !todo.completed })
+            onChange={(e) =>
+              updateTodo.mutate({
+                id: todo.id,
+                completed: e.target.checked,
+              })
             }
           />
 
           <span
-            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+            }}
           >
             {todo.title}
           </span>
